4856d02cdb7e8245482101b057df6b37500d22f1
AlonsoG <alonso.gonzalez.otarola@gmail.com>
2025-09-06 11:45:15 -0400

    redeploy

--- src/components/AddInventoryForm.jsx
+++ src/components/AddInventoryForm.jsx
@@ -634,7 +634,7 @@ const AddInventoryForm = ({
         </DialogActions>
       </Dialog>
 
-      {/* Modal de Error */}
+      {/* Modal de */}
       <Dialog 
         open={showErrorModal} 
         onClose={() => setShowErrorModal(false)}
